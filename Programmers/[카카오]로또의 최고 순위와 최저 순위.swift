eb72c965d46cd4eec8d3ac469435e3484bae2ee2
최윤제 <kong4170@naver.com>
2022-05-16 00:59:25 +0900

    e

--- Programmers/[카카오]로또의 최고 순위와 최저 순위.swift
+++ Programmers/[카카오]로또의 최고 순위와 최저 순위.swift
@@ -3,8 +3,7 @@ import Foundation
 func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
     var score: [Int: Int] = [6:1, 5:2, 4:3, 3:4, 2:5, 1:6, 0:6 ]
     var result = Array(repeating: 0, count: 2)
-    var zero = 0
-    var correct = 0
+    var zero = 0, correct = 0
     
 
     for i in lottos{
