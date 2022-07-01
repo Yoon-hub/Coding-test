371d02d7466bbb2c25d4859226d81d6fe0f8884f
최윤제 <kong4170@naver.com>
2022-07-01 15:58:09 +0900

    연습문제

---
+++ Programmers/서울에서 김서방 찾기.swift
@@ -0,0 +1,10 @@
+func solution(_ seoul:[String]) -> String {
+    var result = 0
+    for i in 0..<seoul.count{
+        if seoul[i] == "Kim"{
+            result = i
+        }
+    }
+    
+    return "김서방은 \(result)에 있다"
+}
