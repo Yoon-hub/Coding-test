d6dfbe99520caed6d689a86af79077e503523533
yoon <kong4170@naver.com>
2022-12-14 01:08:13 +0900

    가장 가까운 같은 글자 수정

--- Programmers/가장 가까운 같은글자.swift
+++ Programmers/가장 가까운 같은글자.swift
@@ -1,30 +1,17 @@
 import Foundation
 
 func solution(_ s:String) -> [Int] {
-    var arr: [String] = []
+    var word: [String: Int] = [:]
     var result: [Int] = []
     
-    for i in 0...s.count-1 {
-        if arr.contains(s[i]!) == false {
-            result.append(-1)
-            arr.append(s[i]!)
+    for (i, val) in s.map {String($0)}.enumerated() {
+        if let beforeIndex = word[val] {
+            result.append(i - beforeIndex)
         } else {
-            var closeIndex = 0
-            for j in 0...arr.count - 1 {
-                if arr[j] == s[i] { closeIndex = j }     
-            }
-            result.append(i - closeIndex)
-            arr.append(s[i]!)
+            result.append(-1)
         }
+        word[val] = i
     }
     
     return result
-}
-
-
-extension String {
-    subscript(idx: Int) -> String? {
-        let result = index(startIndex, offsetBy: idx)
-        return String(self[result])
-    }
 }
